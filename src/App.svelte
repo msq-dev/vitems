443378d1e8499a6983b791a7d785a79ef2401919
msq-dev <63470710+msq-dev@users.noreply.github.com>
2022-12-01 01:46:08 +0100

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { onMount } from "svelte"
+</script>
+
+
+<main>
+  <h1>Tach</h1>
+</main>
+
+
+<style>
+  main {
+    width: min(100% - 2rem, 65ch);
+    margin: 0 auto;
+  }
+</style>
